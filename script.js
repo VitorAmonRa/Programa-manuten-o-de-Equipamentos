98eea5b53fcaedaf54e57dabb1907a8adb33bcdd
Vitor Amon-Ra Fernandes <vitoramon-ra@hotmail.com>
2022-06-14 12:10:47 -0300

    Melhorando Identação

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-/* Usado para padronizar o codigo */
+/* ==== Usado para padronizar o codigo ==== */
 'use strict';
 
-/* Variaveis */
+/* ==== Variaveis ===== */
 let somaRtg = 0;
 let somaSts = 0;
 let somaEv = 0;
@@ -11,51 +11,55 @@ let btnAdicionar = document.getElementById('adicionar');
 let btnRemover = document.getElementById('remover');
 let btnPlanilha = document.getElementById('export');
 
-/* eventos */
+/* ==== Eventos ==== */
 btnAdicionar.addEventListener('click', adicionar,false);
 btnRemover.addEventListener('click', remover,false);
 btnPlanilha.addEventListener('click', exportar,false);
 
-function adicionar(e){
+function adicionar(){
+/* ==== Parte RTG ==== */
     if(document.getElementById('RTG').checked){
         somaRtg = somaRtg + 1;
         document.getElementById('vezesRTG').innerHTML = somaRtg;
+/* ==== Parte STS ==== */
     }else if(document.getElementById('STS').checked){
         somaSts = somaSts + 1;
         document.getElementById('vezesSTS').innerHTML = somaSts;
+ /* ==== Parte EV ==== */
     }else if(document.getElementById('EV').checked){
         somaEv = somaEv + 1;
         document.getElementById('vezesEV').innerHTML = somaEv;
+/* ==== Parte RS ==== */
     }else if(document.getElementById('RS').checked){
         somaRs = somaRs + 1;
         document.getElementById('vezesRS').innerHTML = somaRs;
     }
 }
-function remover(e){
+function remover(){
+/* ==== Parte RTG ==== */
     if(document.getElementById('RTG').checked){
         somaRtg = somaRtg -1;
-         /* Argumento para fazer o valor nao ficar negativo */
          if(somaRtg < 0){
             somaRtg = 0;
         }
         document.getElementById('vezesRTG').innerHTML = somaRtg;
+/* ==== Parte STS ==== */
     }else if(document.getElementById('STS').checked){
         somaSts = somaSts - 1;
-         /* Argumento para fazer o valor nao ficar negativo */
          if(somaSts < 0){
             somaSts = 0;
         }
         document.getElementById('vezesSTS').innerHTML = somaSts;
+/* ==== Parte EV ==== */
     }else if(document.getElementById('EV').checked){
         somaEv = somaEv - 1;
-         /* Argumento para fazer o valor nao ficar negativo */
          if(somaEv < 0){
             somaEv = 0;
         }
         document.getElementById('vezesEV').innerHTML = somaEv;
+/* ==== Parte RS ==== */
     }else if(document.getElementById('RS').checked){
         somaRs = somaRs - 1;
-        /* Argumento para fazer o valor nao ficar negativo */
         if(somaRs < 0){
             somaRs = 0;
         }
